27043881b0c7f1c82957f8f74ac8ac693f83b5c2
Your Name <you@example.com>
2017-05-25 22:54:17 +0300

    z

--- utils.js
+++ utils.js
@@ -31,6 +31,31 @@ textN:   Text data N.
 
 'use strict'
 
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+ */
+if (!String.prototype.startsWith) {
+	String.prototype.startsWith = function(searchString, position){
+	  position = position || 0;
+	  return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+ */
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+	  var subjectString = this.toString();
+	  if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+		position = subjectString.length;
+	  }
+	  position -= searchString.length;
+	  var lastIndex = subjectString.lastIndexOf(searchString, position);
+	  return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
 var FF = isFirefoxBrowser();
 
 var _imgs;
